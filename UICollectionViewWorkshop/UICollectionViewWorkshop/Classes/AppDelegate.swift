651683e0dd0a4bd7d0e15df67e86e385997867fa
Paweł Dudek <pawel@dudek.mobi>
2016-10-30 10:00:10 +0100

    Added style

--- UICollectionViewWorkshop/UICollectionViewWorkshop/Classes/AppDelegate.swift
+++ UICollectionViewWorkshop/UICollectionViewWorkshop/Classes/AppDelegate.swift
@@ -17,16 +17,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
+
+        modifyAppearance()
         
         if device.userInterfaceIdiom == .phone {
             
         } else {
-            
+
         }
         
         window?.rootViewController = UIViewController()
         window?.makeKeyAndVisible()
         return true
+    }
+
+
+    func modifyAppearance() {
+        let workshopBackgroundColor = UIColor.barsBackgroundTint
+        let tintColor = UIColor.text
+
+        UINavigationBar.appearance().barTintColor = workshopBackgroundColor
+        UINavigationBar.appearance().tintColor = tintColor
+        UINavigationBar.appearance().titleTextAttributes = [
+            NSForegroundColorAttributeName : tintColor
+        ]
+
+        UITabBar.appearance().barTintColor = workshopBackgroundColor
+        UITabBar.appearance().tintColor = tintColor
+
+        UIRefreshControl.appearance().tintColor = tintColor
     }
 }
 
---
+++ UICollectionViewWorkshop/UICollectionViewWorkshop/Classes/Generic/Colors.swift
@@ -0,0 +1,17 @@
+//
+// Copyright (©) 2016 Mobile Academy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+
+    class var barsBackgroundTint: UIColor {
+        return UIColor(colorLiteralRed: 66/255, green: 177/255, blue: 211/255, alpha: 1)
+    }
+
+    class var text: UIColor {
+        return UIColor(colorLiteralRed: 52/255, green: 52/255, blue: 52/255, alpha: 1)
+    }
+}
