3fbb73457af35d3e1e5659c8badf60055332d98c
Maciej Oczko <maciejoczko@gmail.com>
2016-10-31 19:37:35 +0100

    Set up speaker cell

--- UICollectionViewWorkshop/UICollectionViewWorkshop/Classes/Speakers/SpeakerCollectionViewCell.swift
+++ UICollectionViewWorkshop/UICollectionViewWorkshop/Classes/Speakers/SpeakerCollectionViewCell.swift
@@ -7,5 +7,25 @@ import Foundation
 import UIKit
 
 final class SpeakerCollectionViewCell: UICollectionViewCell {
+	private(set) lazy var imageView: UIImageView = {
+		let imageView = UIImageView()
+		imageView.contentMode = .scaleAspectFill
+		return imageView
+	}()
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		addSubview(imageView)
+		clipsToBounds = true
+	}
+
+	func configure(with speakerItem: SpeakerItem) {
+		imageView.image = speakerItem.image
+	}
+
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		imageView.frame = bounds
+	}
 
 }
