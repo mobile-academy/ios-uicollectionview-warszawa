f40799077024d9fbff63b5f2a7eb225b2fe171f6
Aleksander Zubala <azubala@applause.com>
2016-11-01 16:07:27 +0100

    Made use of UIMainStoryboardFile~ipad key.

--- UICollectionViewWorkshop/UICollectionViewWorkshop/Classes/AppDelegate.swift
+++ UICollectionViewWorkshop/UICollectionViewWorkshop/Classes/AppDelegate.swift
@@ -11,31 +11,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var device = UIDevice.current
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
         let configuration = ConfigurationFactory().applicationConfiguration()
         Configurator().configureApplication(configuration: configuration, launchOptions: launchOptions)
-        
         modifyAppearance()
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = rootViewController
-        window?.makeKeyAndVisible()
         return true
     }
 
 }
 extension AppDelegate {
-    fileprivate var storyboardName: String {
-        if device.userInterfaceIdiom == .phone {
-            return "Main-Phone"
-        } else {
-            return "Main-Pad"
-        }
-    }
-    
-    fileprivate var rootViewController: UIViewController? {
-        return UIStoryboard(name: storyboardName, bundle: nil).instantiateInitialViewController()
-    }
-
     fileprivate func modifyAppearance() {
         let workshopBackgroundColor = UIColor.barsBackgroundTint
         let tintColor = UIColor.text
