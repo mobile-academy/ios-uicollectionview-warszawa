3e2e4aa653c64172c4ed8c4958c8bf39c031ccb1
Aleksander Zubala <azubala@applause.com>
2016-11-02 21:59:03 +0100

    Added Debug Prase configuration.

--- UICollectionViewWorkshop/UICollectionViewWorkshop/Classes/App Configuration/ConfigurationFactory.swift
+++ UICollectionViewWorkshop/UICollectionViewWorkshop/Classes/App Configuration/ConfigurationFactory.swift
@@ -7,7 +7,7 @@ import Foundation
 class ConfigurationFactory {
 
     func applicationConfiguration() -> Configuration {
-        return Configuration(parseApplicationID:"zNQbE3XfRRFKFtS324xVEQQs0x9usJezmfYqyg7d",
-                parseClientID: "eO6SuMwAPBu1XjJkSkKG5PgTlrPallgv587nOSBn")
+        return Configuration(parseApplicationID:"b15TPKHKv3ggRl9zrEE3UX8ZLWEN6b9eAf4rqzwJ",
+                parseClientID: "C1XY1RmFA7atjhTFZ1ghF7IscSnLIyBQfM6PTot4")
     }
 }
